68a17c3d024bc0215e29baacf7bd80ffadc93d72
Rudy1405 <rudy1405@hotmail.com>
2023-04-24 20:52:56 -0600

    Personalize changes

--- src/mocks/data.ts
+++ src/mocks/data.ts
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 import { OrderStatus } from "~/constants/order";
 import { CartItem } from "~/models/CartItem";
 import { Order } from "~/models/Order";
@@ -8,37 +9,37 @@ export const products: Product[] = [
     description: "Short Product Description1",
     id: "7567ec4b-b10c-48c5-9345-fc73c48a80aa",
     price: 24,
-    title: "ProductOne",
+    title: "Wig One",
   },
   {
     description: "Short Product Description7",
     id: "7567ec4b-b10c-48c5-9345-fc73c48a80a1",
     price: 15,
-    title: "ProductTitle",
+    title: "Afro Wig",
   },
   {
     description: "Short Product Description2",
     id: "7567ec4b-b10c-48c5-9345-fc73c48a80a3",
     price: 23,
-    title: "Product",
+    title: "Mozart Wig",
   },
   {
     description: "Short Product Description4",
     id: "7567ec4b-b10c-48c5-9345-fc73348a80a1",
     price: 15,
-    title: "ProductTest",
+    title: "WigTest",
   },
   {
     description: "Short Product Descriptio1",
     id: "7567ec4b-b10c-48c5-9445-fc73c48a80a2",
     price: 23,
-    title: "Product2",
+    title: "Kanye Wig",
   },
   {
     description: "Short Product Description7",
     id: "7567ec4b-b10c-45c5-9345-fc73c48a80a1",
     price: 15,
-    title: "ProductName",
+    title: "Crisis Britney Wig",
   },
 ];
 
@@ -52,7 +53,7 @@ export const cart: CartItem[] = [
       description: "Short Product Description1",
       id: "7567ec4b-b10c-48c5-9345-fc73c48a80aa",
       price: 24,
-      title: "ProductOne",
+      title: "Wig One",
     },
     count: 2,
   },
@@ -61,7 +62,7 @@ export const cart: CartItem[] = [
       description: "Short Product Description7",
       id: "7567ec4b-b10c-45c5-9345-fc73c48a80a1",
       price: 15,
-      title: "ProductName",
+      title: "Crisis Britney Wig",
     },
     count: 5,
   },
